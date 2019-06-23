0b9f6d1c0ce8d63b8cb168bfbd56e37a4d4a191d
Immanuel von Detten <manu@perspective-daily.de>
2019-06-23 17:50:12 +0200

    feat(init): init

---
+++ app.py
@@ -0,0 +1,10 @@
+from hermes_python.hermes import Hermes
+
+MQTT_ADDR = "localhost:1883"        # Specify host and port for the MQTT broker
+
+def subscribe_weather_forecast_callback(hermes, intent_message):    # Defining callback functions to handle an intent that asks for the weather.
+    print("Parsed intent : {}".format(intent_message.intent.intent_name))
+
+with Hermes(MQTT_ADDR) as h: # Initialization of a connection to the MQTT broker
+    h.subscribe_intent("searchWeatherForecast", subscribe_weather_forecast_callback) \  # Registering callback functions to handle the searchWeatherForecast intent
+         .start()
