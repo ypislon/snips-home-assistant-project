ac6dcffcd8a7d6a08467555f801bebee115cd4d1
Immanuel von Detten <manu@perspective-daily.de>
2019-06-23 18:47:08 +0200

    fff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from hermes_python.hermes import Hermes, MqttOptions
 import json
+import spotipy
 
 MQTT_ADDR = "localhost:1883"        # Specify host and port for the MQTT broker
 
@@ -11,11 +12,17 @@ def subscribe_weather_forecast_callback(hermes, intent_message):    # Defining c
 def subscribe_play_song_callback(hermes, intent_message):
     print("Parsed intent: {}".format(intent_message.intent.intent_name))
     print("slot value: {}".format(intent_message.slots.song_name.first().value))
-    session_id = intent_message.sessionId
+    session_id = intent_message.session_id
     user_input = intent_message.input
+    slot_value = intent_message.slots.song_name.first().value
 
     for slot in intent_message.slots.song_name:
         print("For slot: {}, the confidence is: {}".format(slot.slot_name, slot.confidence_score))
+
+
+    sp = spotipy.Spotify()
+    result = sp.search(intent_message.slots.song_name.first().value)
+    print(result)
 
     return hermes.publish_continue_session(session_id, user_input, [])
 
